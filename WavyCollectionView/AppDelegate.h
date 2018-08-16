f270986fc09b06f898b984e64f135ce47028388b
Bennett <bennettcheung@yahoo.com>
2018-08-15 17:45:21 -0700

    Initial Commit

---
+++ WavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollectionView
+//
+//  Created by Bennett on 2018-08-15.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavyCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WavyCollectionView
+//
+//  Created by Bennett on 2018-08-15.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
